9246a3c503025ef80e46de2fd7cee59eed23cca5
suyash2992 <suyashbhosale29@gmail.com>
2021-05-29 19:02:48 +0530

    Project completed

--- drumOnKeyBoard/drumOnKeyBoard.js
+++ drumOnKeyBoard/drumOnKeyBoard.js
@@ -1,13 +1,21 @@
 function myEvent(event) {
-  console.log(event.keyCode);
-  let getAudioElements = document.getElementById('getAudio');
-  for (let i = 0; i < getAudioElements.children.length; i++) {
-    if (getAudioElements.children[i].getAttribute('id') == event.keyCode) {
-      console.log('Matched');
-      let id = getAudioElements.children[i].getAttribute('id') + '1';
-      console.log(document.getElementById(id));
-      document.getElementById(id).play();
+  let groupOfKeys = document.getElementById('keyHolder');
+  let indivisualKey = groupOfKeys.children;
+  for (let i = 0; i < indivisualKey.length; i++) {
+    if (indivisualKey[i].getAttribute('id') == event.keyCode) {
+      let id = indivisualKey[i].getAttribute('id') + '1';
+      let audio = document.getElementById(id);
+      audio.currentTime = 0;
+      audio.play();
+      indivisualKey[i].classList.add('keyAnimations');
+      function removeTransition(event) {
+        if (event.propertyName == 'width') {
+          indivisualKey[i].classList.remove('keyAnimations');
+        }
+      }
+      indivisualKey[i].addEventListener('transitionend', removeTransition);
     }
   }
 }
+
 window.addEventListener('keydown', myEvent);
